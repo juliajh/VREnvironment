e3ed925047cb6d82bca1e40d16703f5810f52deb
JH Park <54497150+juliajh@users.noreply.github.com>
2022-10-17 22:56:11 +0900

    assets

---
+++ Assets/VRTK/Examples/ExampleResources/Scripts/UI_Keyboard.cs
@@ -0,0 +1,32 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+    using UnityEngine.EventSystems;
+    using UnityEngine.UI;
+
+    public class UI_Keyboard : MonoBehaviour
+    {
+        [SerializeField]
+        private InputField idInput;
+
+        public void ClickKey(string character)
+        {
+            idInput.text += character;
+        }
+
+        public void Backspace()
+        {
+            if (idInput.text.Length > 0)
+            {
+                idInput.text = idInput.text.Substring(0, idInput.text.Length - 1);
+            }
+        }
+
+
+        /*private void Start()
+        {
+            idInput = GetComponentInChildren<InputField>();
+        }*/
+
+    }
+}
---
+++ Assets/test.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    [SerializeField]
+    GameObject whiteImage;
+
+    [SerializeField]
+    GameObject redImage;
+
+    public float force;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RectTransform rt = whiteImage.GetComponent<RectTransform>();
+        Debug.Log(rt.localPosition);
+
+        /*방법1
+        Vector3 distance = new Vector3(rt.localPosition.x, rt.localPosition.y, -30f) - new Vector3(rt.localPosition.x, rt.localPosition.y, 0f);
+        distance = distance.normalized;
+        distance = distance * force;
+        rb.AddForce(distance);
+        */
+
+        rt.localPosition = Vector3.Lerp(rt.localPosition, new Vector3(rt.localPosition.x, rt.localPosition.y, 0f), Time.deltaTime * 5f);
+        rt.localScale = Vector3.Lerp(rt.localScale, new Vector3(2, 2, 2), Time.deltaTime * 5f);
+    }
+}
